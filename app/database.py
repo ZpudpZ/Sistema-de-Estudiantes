9ccae65b49b10aebfbb6b592fce0d01138608697
Wilder <x3wilder@gmail.com>
2026-01-29 09:53:40 -0500

    Arreglar tests usando SQLite y subir dependencias

--- app/database.py
+++ app/database.py
@@ -5,20 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = os.getenv("DB_USER", "user")
-password = os.getenv("DB_PASSWORD", "user_pass")
-host = os.getenv("DB_HOST", "db")
-port = os.getenv("DB_PORT", "3306")
-db_name = os.getenv("DB_NAME", "db_estudiantes")
+if os.getenv("TEST_MODE") == "True":
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+else:
+    user = os.getenv("DB_USER", "user")
+    password = os.getenv("DB_PASSWORD", "user_pass")
+    host = os.getenv("DB_HOST", "db")
+    port = os.getenv("DB_PORT", "3306")
+    db_name = os.getenv("DB_NAME", "db_estudiantes")
+    
+    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Crear sesion (interactuar con la DB)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base para los modelos
 Base = declarative_base()
 
 def get_db():
